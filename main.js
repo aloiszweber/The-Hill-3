50e5dc2efa4dfec705f83e75339589df0b95a33a
becode <alois.zweber@gmail.com>
2022-03-23 11:04:45 +0100

    MAJ2 Adding grid, styling grid and work on the style of the main page

--- main.js
+++ main.js
@@ -1,30 +1,34 @@
 
 
 const getAllCharacter = async () => { 
-    let reponse = await fetch("https://character-database.becode.xyz/characters");
-    let data = await reponse.json();
-    let placeOfHeroes = document.getElementsByTagName('main');
+    let response = await fetch("https://character-database.becode.xyz/characters"); // put the array of my promise from the database in the variable response
+    let data = await response.json(); // put the value (an array) of response with the json format in the data variable
 
-    
-    let character = document.querySelector('.character');
-    console.log(character);
+    var character = document.querySelector('.character'); // selection of the div with character as classname
 
     data.forEach(element => {
 
-        let cloneCharacter = document.importNode(character, true);
+        let cloneCharacter = document.importNode(character, true); // creation of a clone of the div with character as classname
+        // with its children
 
-        let nameCharacter = cloneCharacter.querySelector('.name');
-        let descriptionCharacter = cloneCharacter.querySelector('.description');
+        let nameCharacter = cloneCharacter.querySelector('.name'); // taking of the clone child (tag h3) with name as the classname
+        let descriptionCharacter = cloneCharacter.querySelector('.description'); // taking of the clone child (tag "p") with description as the classname
+        let pictureCharacter = cloneCharacter.querySelector('.pictures'); // taking of the clone child (tag "img") with pictures as the classname
 
-        nameCharacter.innerHTML = element.name;
-        descriptionCharacter.innerHTML = element.description;
 
-        let target = document.getElementById("container");
-        target.appendChild(cloneCharacter);
-        console.log(target);
+        nameCharacter.innerHTML = element.name; // adding of the name of the character from the database to the clone child (classname: name)
+        descriptionCharacter.innerHTML = element.description; // adding of the name of the character from the database to the clone child (classname: description)
+        pictureCharacter.innerHTML = element.pictures // adding of the picture of the character from the database to the clone child (classname: pictures)
+
+
+        let target = document.getElementById("container"); // selection of the container (main content) as a target
+        target.appendChild(cloneCharacter); // adding the clone of character (and its content) to the target (main content)
 
         
     });
+
+    character.style.display = "none";
+
 }
 
 getAllCharacter();
