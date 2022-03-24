b2610b9afdbec506792c5c58b0c55bf154995657
becode <alois.zweber@gmail.com>
2022-03-24 17:15:30 +0100

    MAJ 4 Trying to go on modify.html with the updateBtn button

--- main.js
+++ main.js
@@ -3,7 +3,6 @@
 const getAllCharacter = async () => { 
     let response = await fetch("https://character-database.becode.xyz/characters"); // put the array of my promise from the database in the variable response
     let data = await response.json(); // put the value (an array) of response with the json format in the data variable
-    console.log(data);
 
     var character = document.querySelector('.character'); // selection of the div with character as classname
 
@@ -16,6 +15,7 @@ const getAllCharacter = async () => {
         let descriptionCharacter = cloneCharacter.querySelector('.description'); // taking of the clone child (tag "p") with description as the classname
         let imageCharacter = cloneCharacter.querySelector('.image'); // taking of the clone child (tag "img") with image as the classname
         let link = cloneCharacter.querySelector('a'); // taking of the clone child (tag "a")
+        console.log(link);
 
         nameCharacter.innerHTML = element.name; // adding of the name of the character from the database to the clone child (classname: name)
         descriptionCharacter.innerHTML = element.shortDescription; // adding of the name of the character from the database to the clone child (classname: description)
@@ -25,7 +25,6 @@ const getAllCharacter = async () => {
         let target = document.getElementById("container"); // selection of the container (main content) as a target
         target.appendChild(cloneCharacter); // adding the clone of character (and its content) to the target (main content) 
         
-    
         
     });
 
